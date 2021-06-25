a62a714c8ddadda1ef74b563537794bd26d7dd6a
Amanda <amanda.garrison@improving.com>
2021-06-25 15:23:12 -0500

    rough start

---
+++ ParksApp/Models/ParksAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParksApp
+{
+  
+    public class ParksAPI
+    {
+    private static readonly HttpClient _httpClient= new HttpClient();
+
+        public async Task<string> GetParkName()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://seriouslyfundata.azurewebsites.net/api/parks");
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
